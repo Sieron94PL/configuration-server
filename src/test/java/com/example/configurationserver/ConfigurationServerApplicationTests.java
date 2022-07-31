e14a86c5e85466350486e6723d6ef027946f9fb7
sierodam <damian.sieron@orange.com>
2022-07-31 10:44:00 +0200

    Init commit.

---
+++ src/test/java/com/example/configurationserver/ConfigurationServerApplicationTests.java
@@ -0,0 +1,8 @@
+package com.example.configurationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServerApplicationTests {
+}
